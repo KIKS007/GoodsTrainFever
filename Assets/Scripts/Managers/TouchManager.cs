27030eeb33d7e1d4afc22c175074f9e47fc25ac7
Keryann Malacchina <keryannm@gmail.com>
2017-07-12 00:52:48 +0200

    Working on Inputs

---
+++ Assets/Scripts/Managers/TouchManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum HoldState { None, Holding, SwipingRight, SwipingLeft }
+
+public class TouchManager : Singleton<TouchManager>
+{
+	[Header ("States")]
+	public GameObject target;
+	public HoldState holdState = HoldState.None;
+
+	[Header ("Touch Layer")]
+	public LayerMask touchRaycastLayer;
+
+	private Vector3 _mousePosition;
+	private Vector3 _mouseDeltaPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Application.isEditor)
+			MouseHold ();
+		else
+			TouchHold ();
+	}
+
+	void TouchHold ()
+	{
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+
+			switch (touch.phase)
+			{
+			case TouchPhase.Began:
+				
+				holdState = HoldState.Holding;
+
+				break;
+
+			case TouchPhase.Moved:
+
+				if (touch.deltaPosition.x < 0)
+					holdState = HoldState.SwipingLeft;
+				
+				else if(touch.deltaPosition.x > 0)
+					holdState = HoldState.SwipingRight;
+
+				else
+					holdState = HoldState.Holding;
+				
+				break;
+
+			case TouchPhase.Ended:
+
+				holdState = HoldState.None;
+
+				break;
+			}
+		}
+	}
+
+	void MouseHold ()
+	{
+		if(Input.GetMouseButtonDown (0))
+		{
+			holdState = HoldState.Holding;
+			_mousePosition = Input.mousePosition;
+		}
+
+		if(Input.GetMouseButtonUp (0))
+			holdState = HoldState.None;
+
+		if(Input.GetMouseButton (0))
+		{
+			_mouseDeltaPosition = Input.mousePosition - _mousePosition; 
+
+			if (_mouseDeltaPosition.x < 0)
+				holdState = HoldState.SwipingLeft;
+			
+			else if (_mouseDeltaPosition.x > 0)
+				holdState = HoldState.SwipingRight;
+
+			else
+				holdState = HoldState.Holding;
+			
+			_mousePosition = Input.mousePosition;
+		}
+	}
+}
---
+++ Assets/Scripts/Touchable.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Touchable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	protected float _holdDelay = 0.1f;
+	protected bool _pointerDown = false;
+	protected bool _holding = false;
+
+	// Update is called once per frame
+	protected virtual void Update () 
+	{
+		if (_pointerDown)
+			OnTouch ();
+
+		if (_holding)
+			OnTouchHold ();
+	}
+
+	protected virtual void OnTouchDown ()
+	{
+		Debug.Log ("OnTouchDown");
+	}
+
+	protected virtual void OnTouch ()
+	{
+		Debug.Log ("OnTouch");
+	}
+
+	protected virtual void OnTouchHold ()
+	{
+		Debug.Log ("OnTouchHold");
+	}
+
+	protected virtual void OnTouchUp ()
+	{
+		Debug.Log ("OnTouchUp");
+	}
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		OnTouchDown ();
+		StartCoroutine (HoldDelay ());
+		_pointerDown = true;
+		_holding = false;
+	}
+
+	public void OnPointerUp (PointerEventData eventData)
+	{
+		OnTouchUp ();
+		StopCoroutine (HoldDelay ());
+		_pointerDown = false;
+		_holding = false;
+	}
+
+	protected IEnumerator HoldDelay ()
+	{
+		yield return new WaitForSecondsRealtime (_holdDelay);
+
+		if (_pointerDown)
+			_holding = true;
+	}
+}
