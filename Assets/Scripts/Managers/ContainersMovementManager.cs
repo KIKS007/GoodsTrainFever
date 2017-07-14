02b371999c42eb475af216cd7c94e2c25d13f14c
Keryann Malacchina <keryannm@gmail.com>
2017-07-14 20:49:19 +0200

    Changed TakeSpot Anims

--- Assets/Scripts/Managers/ContainersMovementManager.cs
+++ Assets/Scripts/Managers/ContainersMovementManager.cs
@@ -20,6 +20,9 @@ public class ContainersMovementManager : Singleton<ContainersMovementManager>
 	[Header ("Stop Hover")]
 	public float stopHoverDuration = 0.2f;
 
+	[Header ("Take Spot")]
+	public float takeSpotDuration = 0.4f;
+
 	private float _startHeight;
 
 	public void StartHover (Container container)
@@ -59,24 +62,26 @@ public class ContainersMovementManager : Singleton<ContainersMovementManager>
 
 		container.transform.DOKill (false);
 
-		Vector3 rotationDir;
+		Vector3 direction = (spot.transform.position - container.transform.position).normalized;
 
-		rotationDir = Vector3.forward;
-		Vector3 diff = Vector3.Scale (container.transform.position, InverseVector (rotationDir)) - Vector3.Scale (spot.transform.position, InverseVector (rotationDir));
+		if(VectorApproximatelyEqual (Vector3.right, direction) || VectorApproximatelyEqual (-Vector3.right, direction))
+			direction = Vector3.forward * -Mathf.Sign (direction.x);
+		else
+			direction = Vector3.right * Mathf.Sign (direction.z);
 
 		ScreenshakeManager.Instance.Shake (FeedbackType.StartTakeSpot);
 
 		//360 Rotation
-		container.transform.DORotate (rotationDir * Mathf.Sign (diff.x + diff.y + diff.z) * 360f, .4f, RotateMode.FastBeyond360);
+		//container.transform.DORotate (rotationDir * Mathf.Sign (diff.x + diff.y + diff.z) * 360f, .4f, RotateMode.FastBeyond360);
+		container.transform.DORotate (direction * 360f, .4f, RotateMode.FastBeyond360);
 
 		container.transform.DOMoveX (spot.transform.position.x, .4f).SetEase (Ease.OutCubic);
 		container.transform.DOMoveZ (spot.transform.position.z, .4f).SetEase (Ease.OutCubic);
 		container.transform.DOMoveY (spot.transform.position.y + 10f + Random.Range (-2, 3), .3f).SetEase (Ease.OutCubic).OnComplete (() => 
 			{
 				container.transform.DOMoveY (spot.transform.position.y, .5f).SetEase (Ease.OutBounce, 40, 1);
-				container.transform.DOPunchRotation (rotationDir * Mathf.Sign (diff.x + diff.y + diff.z) * 10f, .5f, 10).SetDelay (.1f).OnStart (() => 
+				container.transform.DOPunchRotation (direction * 10f, .5f, 10).SetDelay (.1f).OnStart (() => 
 					{
-						
 						//DOVirtual.DelayedCall (.2f, () => ParticlesManager.Singleton.Create (ParticlesManager.Singleton.FXDropFog, spot.transform.position - (Vector3.up * colliderCp.bounds.extents.y * .5f)));
 						
 						ScreenshakeManager.Instance.Shake (FeedbackType.EndTakeSpot);
@@ -84,12 +89,8 @@ public class ContainersMovementManager : Singleton<ContainersMovementManager>
 			});
 	}
 
-	Vector3 InverseVector (Vector3 vector)
+	bool VectorApproximatelyEqual (Vector3 a, Vector3 b)
 	{
-		Vector3 v = vector;
-		v.x = Mathf.Abs (v.x - 1);
-		v.y = Mathf.Abs (v.y - 1);
-		v.z = Mathf.Abs (v.z - 1);
-		return v;
+		return Vector3.SqrMagnitude (a - b) < 1f;
 	}
 }
