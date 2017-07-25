9c70f05be9765932de61d1e32633a0d36b946144
valgoun <valgoun@gmail.com>
2017-07-25 15:31:08 +0200

    Custom Import Setting for Fbx

---
+++ Assets/Editor/CustomFbxImporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Custom class to change default import setting for fbx files
+/// </summary>
+public class CustomFbxImporter : AssetPostprocessor
+{
+    private void OnPreprocessModel()
+    {
+
+        ModelImporter Importer = assetImporter as ModelImporter;
+        Importer.importMaterials = false;
+    }
+}
