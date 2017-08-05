736403326d9cd6f1a30fce3afe0192bbc3002edc
Keryann Malacchina <keryannm@gmail.com>
2017-08-05 15:55:00 +0200

    Wokring On Train LD

---
+++ Assets/Scripts/Level Design/Train_LD.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train_LD : ScriptableObject
+{
+	public List<Wagon_Level> wagons;
+	public List<Container_Level> containers = new List<Container_Level> ();
+
+}
--- Assets/Scripts/Managers/GlobalVariables.cs
+++ Assets/Scripts/Managers/GlobalVariables.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using Sirenix.OdinInspector;
 using System;
+using System.IO;
+using UnityEditor;
 
 public class GlobalVariables : Singleton<GlobalVariables>
 {
@@ -34,6 +36,9 @@ public class GlobalVariables : Singleton<GlobalVariables>
 	[Header ("Spawn Spots Prefabs")]
 	public GameObject spot40SpawnedPrefab;
 
+	[Header ("Scriptable Object")]
+	public UnityEngine.Object objectToCreate;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -50,4 +55,38 @@ public class GlobalVariables : Singleton<GlobalVariables>
 	{
 		fpsText.text = ((int)1.0f / Time.smoothDeltaTime).ToString ("##.00");
 	}
+
+	#if UNITY_EDITOR
+	void CreateScriptableObject ()
+	{
+		//CreateAsset<objectToCreate.GetType ()> ();
+	}
+
+	[Button]
+	public void CreateAsset ()
+	{
+		//var type = objectToCreate.GetType ().Name;
+
+		var asset = ScriptableObject.CreateInstance (objectToCreate.name);
+
+		string path = AssetDatabase.GetAssetPath (Selection.activeObject);
+		if (path == "") 
+		{
+			path = "Assets";
+		} 
+		else if (Path.GetExtension (path) != "") 
+		{
+			path = path.Replace (Path.GetFileName (AssetDatabase.GetAssetPath (Selection.activeObject)), "");
+		}
+
+		string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath (path + "/New " + objectToCreate.name + ".asset");
+
+		AssetDatabase.CreateAsset (asset, assetPathAndName);
+
+		AssetDatabase.SaveAssets ();
+		AssetDatabase.Refresh();
+		EditorUtility.FocusProjectWindow ();
+		Selection.activeObject = asset;
+	}
+	#endif
 }
