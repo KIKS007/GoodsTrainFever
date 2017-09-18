442b3a22a69791195975fbecb33e2cef75b8c980
EnolCook <n.grange@rubika-edu.com>
2017-09-18 16:50:01 +0200

    WIP LD Tool

---
+++ Assets/Scripts/Localization/SpreadsheetImporter.cs
@@ -0,0 +1,223 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using Spreadsheet;
+
+
+
+[ExecuteInEditMode]
+public class SpreadsheetImporter : MonoBehaviour {
+    [Header ("Configuration")]
+    public string SheetID;
+
+    public static SpreadsheetImporter Singleton;
+    [Header ("Other")]
+    public bool sync;
+    public bool refresh;
+    public bool fetch;
+    public string CurrentLang = "English";
+
+    public List<string> LanguagesName = new List<string> ();
+    public Dictionary<string, Language> Languages = new Dictionary<string, Language> ();
+
+    public Dictionary<string, string> ColumnToLangName = new Dictionary<string, string> ();
+    public Dictionary<string, string> RowToTextID = new Dictionary<string, string> ();
+    // Use this for initialization
+
+    [ExecuteInEditMode]
+    void Awake () {
+        if (SpreadsheetImporter.Singleton == null) {
+            SpreadsheetImporter.Singleton = this;
+        } else {
+            Destroy (gameObject);
+        }
+    }
+
+    [ExecuteInEditMode]
+    public string GetText (string key) {
+
+        if (LanguagesName.IndexOf (CurrentLang) == -1)
+            return "BAD LANG";
+
+        if (!Languages[CurrentLang].Texts.ContainsKey (key))
+            return "BAD KEY";
+
+        return Languages[CurrentLang].Texts[key];
+    }
+
+    [ExecuteInEditMode]
+    void FetchLanguages () {
+        UnityEngine.Object[] langs = Resources.LoadAll ("lang");
+        if (langs != null) {
+            for (int i = 0; i <= langs.Length - 1; i++) {
+                if ((TextAsset) langs[i]) {
+                    TextAsset ta = (TextAsset) langs[i];
+                    LanguagesName.Add (ta.name);
+                }
+            }
+        }
+    }
+
+    [ExecuteInEditMode]
+    public event MyDel OnLangChange;
+
+    [ExecuteInEditMode]
+    public void ChangeLanguage (string lang) {
+        if (LanguagesName.IndexOf (lang) == -1)
+            return;
+
+        CurrentLang = lang;
+        Load (CurrentLang);
+
+        if (OnLangChange != null)
+            OnLangChange ();
+
+    }
+
+    [ExecuteInEditMode]
+    void Load (string LangName) {
+        TextAsset ta = Resources.Load<TextAsset> ("lang/" + LangName);
+        if (ta != null) {
+            Language lang = JsonUtility.FromJson<Language> (ta.text);
+            for (int j = 0; j <= lang.textsDictionary.Count - 1; j++) {
+                lang.Texts.Add (lang.textsDictionary[j].key, lang.textsDictionary[j].value);
+            }
+
+            if (Languages.ContainsKey (lang.name)) {
+                Languages.Remove (lang.name);
+            }
+
+            if (LanguagesName.IndexOf (lang.name) == -1) {
+                LanguagesName.Add (lang.name);
+            }
+
+            Languages.Add (lang.name, lang);
+
+        }
+    }
+
+    [ExecuteInEditMode]
+    public IEnumerator Fetch () {
+#if UNITY_EDITOR
+        if (sync) {
+
+            WWW www = new WWW ("https://spreadsheets.google.com/feeds/cells/" + SheetID + "/1/public/values?alt=json");
+            yield return www;
+
+            /*
+             * Ensure url exsists (fetch all url)
+             */
+            if (www.responseHeaders["STATUS"] != "HTTP/1.1 400 Bad Request") {
+                Languages.Clear ();
+                RowToTextID.Clear ();
+                ColumnToLangName.Clear ();
+                LanguagesName.Clear ();
+
+                Sheet t = JsonUtility.FromJson<Sheet> (www.text.Trim ().Replace ("$", "__"));
+
+                // FENO'S MAGIC
+                foreach (Entry e in t.feed.entry) {
+                    string column = e.title.__t;
+                    string row = System.Text.RegularExpressions.Regex.Match(column, @"\d+").Value+"";
+                    column = column.Replace(row, "")+"";
+
+                    // Debug.Log(column);
+                    // Debug.Log(e.title.__t[1] +"|||"+ e.title.__t.Length);
+
+                    // Debug.Log(e.title.__t[0]);
+                    if (row == "1") {
+                        Language l = new Language ();
+                        l.name = (string) e.content.__t;
+                        l.id = e.title.__t[0];
+                        ColumnToLangName.Add (column, (string) e.content.__t);
+                        Languages.Add ((string) e.content.__t, l);
+                        LanguagesName.Add ((string) e.content.__t);
+                    } else if (column == "A" ) {
+                        RowToTextID.Add (row, (string) e.content.__t);
+
+                        foreach (KeyValuePair<string, Language> entry in Languages) {
+                            Languages[entry.Key].Texts.Add ((string) e.content.__t, "");
+                        }
+                    } else {
+                        // Debug.Log(column);
+                        // Debug.Log(ColumnToLangName[column]);
+
+                        Languages[ColumnToLangName[column]].Texts[RowToTextID[row]] = e.content.__t;
+                        TextsDictionary td = new TextsDictionary ();
+                        td.key = RowToTextID[row];
+                        td.value = e.content.__t;
+                        Languages[ColumnToLangName[column]].textsDictionary.Add (td);
+                    }
+                }
+
+                // SAVING FILE
+                foreach (KeyValuePair<string, Language> entry in Languages) {
+                    System.IO.File.WriteAllText ("Assets/Resources/lang/" + entry.Value.name + ".json", JsonUtility.ToJson (Languages[entry.Key]));
+                }
+                Debug.Log ("Spreadsheet synced");
+                AssetDatabase.Refresh ();
+                ChangeLanguage (CurrentLang);
+            }
+        }
+#endif
+        yield return null;
+    }
+
+    [ExecuteInEditMode]
+    void Start () {
+        Languages.Clear ();
+        RowToTextID.Clear ();
+        ColumnToLangName.Clear ();
+        LanguagesName.Clear ();
+
+        FetchLanguages ();
+        Load (CurrentLang);
+        /*
+		 * LOAD ALL
+		UnityEngine.Object[] langs = Resources.LoadAll ("lang"); 
+		if (langs != null) {
+			for (int i = 0; i <= langs.Length - 1; i++) {
+				if ((TextAsset) langs [i]) {
+					TextAsset ta = (TextAsset) (langs [i]);
+					Language lang = JsonUtility.FromJson<Language> (ta.text);
+					for (int j = 0; j <= lang.textsDictionary.Count - 1; j++) {
+						lang.Texts.Add (lang.textsDictionary [j].key, lang.textsDictionary [j].value);
+					}
+					Languages.Add (lang.name, lang);
+					LanguagesName.Add (lang.name);
+				}
+			}
+		}
+*/
+
+        StartCoroutine (Fetch ());
+
+        //		yield return null;
+    }
+
+#if UNITY_EDITOR
+    // Update is called once per frame
+    [ExecuteInEditMode]
+    void Update () {
+
+        if (refresh) {
+            ChangeLanguage (CurrentLang);
+            refresh = false;
+        }
+
+        if (fetch) {
+            StartCoroutine (Fetch ());
+            fetch = false;
+        }
+    }
+#endif
+}
+
+//{"appid":730,"name":"Counter-Strike: Global Offensive","developer":"Valve","publisher":"Valve","score_rank":84,"owners":22796584,"owners_variance":118099,"players_forever":22079017,"players_forever_variance":116367,"players_2weeks":8212915,"players_2weeks_variance":72623,"average_forever":15526,"average_2weeks":882,"median_forever":4300,"median_2weeks":398,"ccu":593940}
---
+++ Assets/Scripts/Other/GetLDValues.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+public class GetLDValues : MonoBehaviour {
+
+[Button]
+void GetValue(){
+	Transform parent = this.transform;
+
+	string things;
+	string at1 = "";
+	string at2 = "";
+	string at3 = "";
+	string at4 = "";
+	string at5 = "";
+	foreach(Transform t in parent){
+		if(t.gameObject.GetComponent<LevelHandmade>()){
+		LevelHandmade lhm =  t.gameObject.GetComponent<LevelHandmade>();
+		int t1 =0;
+
+		foreach(Train_Level tl in lhm.rail1Trains){
+			t1 += tl.trainDuration;
+		}
+		foreach(Train_Level tl in lhm.rail2Trains){
+			t1 += tl.trainDuration;
+		}
+		var t2 = lhm.orders.Count;
+		var t3 = lhm.rail1Trains.Count + lhm.rail2Trains.Count;
+		var t4 = lhm.boats.Count;
+		var t5 = lhm.errorsAllowed;
+		at1 += t1 +"	";
+		at2 += t2 +"	";
+		at3 += t3 +"	";
+		at4 += t4 +"	";
+		at5 += t5 +"	";
+		}
+
+
+		if(t.gameObject.GetComponent<LevelSettings_LD>()){
+		LevelSettings_LD lsld =  t.gameObject.GetComponent<LevelSettings_LD>();
+		var t1 = lsld.levelDuration;
+		var t2 = "["+lsld.ordersCountMin+","+lsld.ordersCountMax+"]";
+		var t3 = "["+lsld.trainsCountMin+","+lsld.trainsCountMax+"]";
+		var t4 = "NO";
+		var t5 = lsld.errorsAllowed;
+		at1 += t1 +"	";
+		at2 += t2 +"	";
+		at3 += t3 +"	";
+		at4 += t4 +"	";
+		at5 += t5 +"	";
+		}
+	}
+	things = at1 + "\n" + at2 + "\n" + at3 + "\n" + at4 + "\n" + at5;
+	
+	System.IO.File.WriteAllText ("Assets/" + "LDValue" + ".txt", things);
+}
+
+
+
+}
