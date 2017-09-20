bd1fc5d86d23eb4acf20d6d3f229cda77f790b18
EnolCook <n.grange@rubika-edu.com>
2017-09-20 14:57:43 +0200

    Update on Play Store - Public Beta

--- Assets/Scripts/Level Design/LevelSettings_Sync.cs
+++ Assets/Scripts/Level Design/LevelSettings_Sync.cs
@@ -3,56 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
-public class LevelSettings_Sync : MonoBehaviour {
+public class LevelSettings_Sync : MonoBehaviour
+{
 
 
 	public Transform lvlparent;
 	[HideInInspector]
 	public bool Wait = false;
+	#if UNITY_EDITOR
 	[Button]
 	[ExecuteInEditMode]
-	void SyncLDValues(){
-	if(SpreadsheetImporter.Singleton != null){
-			StartCoroutine(Fetching());
-	}else{
-		this.gameObject.GetComponent<SpreadsheetImporter>().MakeInstance();
-		StartCoroutine(Fetching());
-	}
+	void SyncLDValues ()
+	{
+		if (SpreadsheetImporter.Singleton != null) {
+			StartCoroutine (Fetching ());
+		} else {
+			this.gameObject.GetComponent<SpreadsheetImporter> ().MakeInstance ();
+			StartCoroutine (Fetching ());
+		}
 
 	}
 
 	[ExecuteInEditMode]
-	IEnumerator Fetching(){
+	IEnumerator Fetching ()
+	{
 		SpreadsheetImporter.Singleton.isFetched = false;
 		SpreadsheetImporter.Singleton.sync = true;
-		SpreadsheetImporter.Singleton.FetchLD(this);
-		yield return new WaitUntil(() => Wait);
+		SpreadsheetImporter.Singleton.FetchLD (this);
+		yield return new WaitUntil (() => Wait);
 		Wait = false;
 
-		foreach(Transform t in lvlparent){
-			if(t.gameObject.GetComponent<LevelSettings_LD>()){
-			LevelSettings_LD lsld =  t.gameObject.GetComponent<LevelSettings_LD>();
-				string row = System.Text.RegularExpressions.Regex.Match(lsld.name, @"\d+").Value+"";
-				SpreadsheetImporter.Singleton.ChangeLanguage(row);
+		foreach (Transform t in lvlparent) {
+			if (t.gameObject.GetComponent<LevelSettings_LD> ()) {
+				LevelSettings_LD lsld = t.gameObject.GetComponent<LevelSettings_LD> ();
+				string row = System.Text.RegularExpressions.Regex.Match (lsld.name, @"\d+").Value + "";
+				SpreadsheetImporter.Singleton.ChangeLanguage (row);
 				
 				
-				int.TryParse( SpreadsheetImporter.Singleton.GetText("Duration"), out lsld.levelDuration);
-				int.TryParse( SpreadsheetImporter.Singleton.GetText("Errors"), out lsld.errorsAllowed);
+				int.TryParse (SpreadsheetImporter.Singleton.GetText ("Duration"), out lsld.levelDuration);
+				int.TryParse (SpreadsheetImporter.Singleton.GetText ("Errors"), out lsld.errorsAllowed);
 				
 
-				string t1 = SpreadsheetImporter.Singleton.GetText("Orders Count");
-				string t2 = SpreadsheetImporter.Singleton.GetText("Trains Count");
-				t1 = t1.Replace("[","");
-				t1 = t1.Replace("]","");
-				string[] t1tmp = t1.Split(',');
-				int.TryParse( t1tmp[0], out lsld.ordersCountMin);
-				int.TryParse( t1tmp[1], out lsld.ordersCountMax);
+				string t1 = SpreadsheetImporter.Singleton.GetText ("Orders Count");
+				string t2 = SpreadsheetImporter.Singleton.GetText ("Trains Count");
+				t1 = t1.Replace ("[", "");
+				t1 = t1.Replace ("]", "");
+				string[] t1tmp = t1.Split (',');
+				int.TryParse (t1tmp [0], out lsld.ordersCountMin);
+				int.TryParse (t1tmp [1], out lsld.ordersCountMax);
 
-				t2 = t2.Replace("[","");
-				t2 = t2.Replace("]","");
-				string[] t2tmp = t2.Split(',');
-				int.TryParse( t2tmp[0], out lsld.trainsCountMin);
-				int.TryParse( t2tmp[1], out lsld.trainsCountMax);
+				t2 = t2.Replace ("[", "");
+				t2 = t2.Replace ("]", "");
+				string[] t2tmp = t2.Split (',');
+				int.TryParse (t2tmp [0], out lsld.trainsCountMin);
+				int.TryParse (t2tmp [1], out lsld.trainsCountMax);
 
 				
 
@@ -62,5 +66,5 @@ public class LevelSettings_Sync : MonoBehaviour {
 		SpreadsheetImporter.Singleton.isFetched = false;
 		SpreadsheetImporter.Singleton.sync = false;
 	}
-
+	#endif
 }
