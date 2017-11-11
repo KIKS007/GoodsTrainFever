8e793bba379e6c4508bd0ce03c79ead00c09a89f
EnolCook <n.grange@rubika-edu.com>
2017-11-11 23:07:09 +0100

    Using LUT in Amplify Color instead of Color Grading
    Make the game uglier but make it compatible with many new devices. We can change the LUT file to make it less ugly.

---
+++ Assets/Scripts/Other/PostProcessSwap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class PostProcessSwap : MonoBehaviour
+{
+	[SerializeField]
+	private PostProcessingBehaviour PPB;
+	[SerializeField]
+	private AmplifyColorEffect ACE;
+	private bool DoOnce;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (!DoOnce) {
+			DoOnce = true;
+			if (SystemInfo.SupportsTextureFormat (TextureFormat.RGBAHalf)) {
+				PPB.enabled = true;
+				ACE.enabled = false;
+			} else {
+				PPB.enabled = false;
+				ACE.enabled = true;
+			}
+		}
+
+	}
+}
